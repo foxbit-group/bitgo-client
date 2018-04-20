8f8e292286bfc5a36f001d0516515cc073379830
Bruno Soares <bruno@bsoares.com.br>
2018-04-20 20:29:18 -0300

    Setup SimpleCov

--- bitgo-client.gemspec
+++ bitgo-client.gemspec
@@ -29,4 +29,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rubocop",                   "~> 0.53"
   spec.add_development_dependency "rubocop-github",            "~> 0.10"
   spec.add_development_dependency "rubocop-rspec",             "~> 1.24"
+  spec.add_development_dependency "simplecov",                 "~> 0.16"
+  spec.add_development_dependency "simplecov-console",         "~> 0.4"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,20 @@ require "bitgo_client"
 
 require "bundler/setup"
 require "pry-byebug"
+require "simplecov"
+require "simplecov-console"
+
+Dir["./spec/support/**/*.rb"].each { |file| require file }
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 end
+
+SimpleCov.formatter = SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
