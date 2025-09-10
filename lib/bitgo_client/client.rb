8dd17ebaef0ae154afdd7aa73ba3d98a2e216b61
Victor Giraldes <victor.giraldes@foxbit.com.br>
2025-09-10 15:47:47 -0300

    Improve bitgo client performance (#26)
    * Improve performance by disable memoize
    
    * Set memoization and cache false

--- lib/bitgo_client/client.rb
+++ lib/bitgo_client/client.rb
@@ -28,6 +28,9 @@ module BitgoClient
     end
 
     def request(url, payload = nil, method: :get, logger: nil)
+      Typhoeus::Config.memoize = false
+      Typhoeus::Config.cache   = nil
+
       body = payload.to_json if payload
 
       log logger, "Request url: #{url}, method: #{method}, body:"
